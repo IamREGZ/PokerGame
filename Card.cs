11a62796e551696f9c4b98564ac5fca27b8e795f
IamREGZ <IamREGZSANTOS@gmail.com>
2024-01-30 08:36:11 +0800

    Add project files.

---
+++ Card.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerGame
+{
+    class Card
+    {
+        private string value;
+        private string suit;
+
+        public Card(string value, string suit)
+        {
+            this.value = value;
+            this.suit = suit;
+        }
+
+        public string GetCardValue()
+        {
+            return value;
+        }
+
+        public void SetCardValue(string value)
+        {
+            this.value = value;
+        }
+
+        public string GetCardSuit()
+        {
+            return suit;
+        }
+
+        public void SetCardSuit(string suit)
+        {
+            this.suit = suit;
+        }
+    }
+}
